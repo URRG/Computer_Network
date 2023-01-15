17b4a85bebe8665eef5552ddcd52214239d12b3d
Utkarsh Rastogi <95937956+URRG@users.noreply.github.com>
2023-01-15 20:01:32 +0530

    Create Bit_stuffing.c

---
+++ Bit_stuffing.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i=0,j,x,pos;
+    printf("Enter the size of bit strip\n");
+    scanf("%d",&n);
+    int a[n];
+    for(;i<=n;i++){
+        printf("enter the value 0 or 1 - ");
+        scanf("%d",&j);
+        a[i]=j;
+    }
+    
+    for(i=0;i<=n;i++){
+        printf("[%d]",a[i]);
+    }
+    printf("\n");
+    int m=0,count=0;
+    for(m=0;m<=n;m++){
+        if (a[m]==1){
+        count=count+1;
+           if(count==6){
+                x=0;
+                pos=m;
+                n++;
+                for (m = n; m >= pos; m--){
+                    a[m] = a[m - 1];
+                }
+                count=0;
+                a[pos] = x;
+            }
+        }
+        if(a[m]==0){
+            count=0;
+        }
+    
+    }
+    for(i=0;i<=n;i++){
+        printf("[%d]",a[i]);
+    }
+    return 0;
+}
