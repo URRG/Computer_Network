8de91d4642987523dfb58ffc72ff2bcc8116412a
Utkarsh Rastogi <95937956+URRG@users.noreply.github.com>
2023-05-05 23:25:23 +0530

    Update and rename and_or_nand_gates to and_or_nand_gates.cpp

---
+++ and_or_nand_gates.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a,b,c;
+cout<<"Enter the choice in number\n1.And\n2.OR\n3.XOR";
+cin>>a;
+cout<<"enter first number";
+cin>>b;
+cout<<"enter second number";
+cin>>c;
+   
